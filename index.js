ff5df7784db237bf2c3e52b540920000ebda3657
IndratejReddy <indratejreddy@gmail.com>
2023-05-27 21:56:34 +0530

    added envs

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const getTechStack = require("./routes/getTechStack.js")
-const getMultiMedia = require("./routes/getMultiMedia.js")
+const getTechStack = require("./routes/getTechStack.js");
+const getMultiMedia = require("./routes/getMultiMedia.js");
 
 const app = express();
 app.use(cors());
 
 // Route with query parameters
-app.get('/getTechStack', async (req, res) => {
+app.get("/getTechStack", async (req, res) => {
   const url = req.query.url;
 
-  const output = await getTechStack(url)
-  res.json(output.data)
+  const output = await getTechStack(url, process.env.Gen_Key);
+  res.json(output.data);
 });
 
-app.get('/getMultiMedia', async (req, res) => {
+app.get("/getMultiMedia", async (req, res) => {
   const url = req.query.url;
-  console.log(url)
-  const output = await getMultiMedia(url)
-  res.json(output.data)
+  console.log(url);
+  const output = await getMultiMedia(url);
+  res.json(output.data);
 });
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
--- routes/getTechStack.js
+++ routes/getTechStack.js
@@ -1,22 +1,22 @@
-const axios = require("axios")
+const axios = require("axios");
 
-async function getTechStack(url) {
-    const options = {
-        method: "GET",
-        url: "https://www.genelify.com/api/v1/technology-lookup",
-        params: { url: url },
-        headers: {
-            "Content-Type": "application/json",
-            "X-GENELIFY-API-KEY": "c7e690bd3e1f68da0aaf8fe20be9e418045a0287062feec8e8725b420c2f2890",
-        },
-    }
+async function getTechStack(url, key) {
+  const options = {
+    method: "GET",
+    url: "https://www.genelify.com/api/v1/technology-lookup",
+    params: { url: url },
+    headers: {
+      "Content-Type": "application/json",
+      "X-GENELIFY-API-KEY": key,
+    },
+  };
 
-    try {
-        const response = await axios.request(options)
-        return response
-    } catch (error) {
-        return error
-    }
+  try {
+    const response = await axios.request(options);
+    return response;
+  } catch (error) {
+    return error;
+  }
 }
 
-module.exports = getTechStack
+module.exports = getTechStack;
