b89e0e2d642859e235dcbd6d4d87b4fdab93a981
Indratej Reddy <66834653+Indratejreddy@users.noreply.github.com>
2023-05-27 21:10:34 +0530

    Update getTechStack.js

--- routes/getTechStack.js
+++ routes/getTechStack.js
@@ -1,24 +1,22 @@
-require("dotenv").config();
-
-const axios = require("axios");
+const axios = require("axios")
 
 async function getTechStack(url) {
-  const options = {
-    method: "GET",
-    url: "https://www.genelify.com/api/v1/technology-lookup",
-    params: { url: url },
-    headers: {
-      "Content-Type": "application/json",
-      "X-GENELIFY-API-KEY": process.env.X_GENELIFY_API_KEY,
-    },
-  };
+    const options = {
+        method: "GET",
+        url: "https://www.genelify.com/api/v1/technology-lookup",
+        params: { url: url },
+        headers: {
+            "Content-Type": "application/json",
+            "X-GENELIFY-API-KEY": "c7e690bd3e1f68da0aaf8fe20be9e418045a0287062feec8e8725b420c2f2890",
+        },
+    }
 
-  try {
-    const response = await axios.request(options);
-    return response;
-  } catch (error) {
-    return error;
-  }
+    try {
+        const response = await axios.request(options)
+        return response
+    } catch (error) {
+        return error
+    }
 }
 
-module.exports = getTechStack;
+module.exports = getTechStack
